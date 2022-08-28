779a6638743e361a124bcdd2556999f0cca37d37
Rafael Morais <rdemorais.freitas@gmail.com>
2022-08-28 17:34:37 -0300

    inclusão de base-config

---
+++ scripts/convert_hcpa_cn_to_spacy.py
@@ -0,0 +1,18 @@
+from spacy.tokens import DocBin
+from wasabi import Printer
+import spacy
+
+from pathlib import Path
+import typer
+import csv
+
+msg = Printer()
+
+
+def main(corpus_pristine: Path, corpus_spacy: Path):
+  pass
+
+
+if __name__ == "__main__":
+  typer.run(main)
+
